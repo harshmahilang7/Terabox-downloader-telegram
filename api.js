fe859933394711e1e6118c02e8ba2c53fd01a6a9
HARSH MAHILANG <166123573+harshmahilang7@users.noreply.github.com>
2025-04-16 00:35:26 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function getDetails(id) {
     try {
         const response = await axios.get(
-            `https://terabox-dl-arman.vercel.app/api?data=${id}`
+            `https://tera.ronok.workers.dev/?link=${id}&apikey=0b010c132e2cbd862cbd8a6ae430dd51d3a0d5ea`
         );
         return response.data;
     } catch (error) {
